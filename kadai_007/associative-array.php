b633b718b867405b73e7350ad1f0bcc5c6e04b46
morita <99958533+Mor1Sh0@users.noreply.github.com>
2024-12-16 21:21:32 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>テストの平均点を計算しよう</title>
+</head>
+
+<body>
+    <!-- PHPの埋め込み -->
+    <?php
+    $personal_data = ['name' => 'onion', 'price' => '200', 'weight' => 160];
+    print_r($personal_data);
+    ?>
+</body>
+
+</html>
