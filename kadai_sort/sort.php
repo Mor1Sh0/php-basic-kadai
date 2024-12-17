8ba80ada2a6874bc7e92d6dbac3bed62c0463e0a
morita <99958533+Mor1Sh0@users.noreply.github.com>
2024-12-17 10:38:41 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,29 +10,26 @@
     <p>
         <?php
         $nums = [15, 4, 18, 23, 10];
+
         function sort_2way($array,$order){
             if($order){
                 echo '昇順にソートします。'.'<br>';
                 sort($array);
-                return $array;
             }else{
                 echo '降順にソートします。'.'<br>';
                 rsort($array);
-                return $array;
+            }
+
+            foreach($array as $value){
+                echo $value.'<br>';
             }
         }
 
-        $asc = sort_2way($nums, true);
+        sort_2way($nums, true);
 
-        foreach($asc as $value){
-            echo $value.'<br>';
-        }
+        sort_2way($nums, false);
 
-        $desc = sort_2way($nums, false);
 
-        foreach($desc as $value){
-            echo $value.'<br>';
-        }
 
         ?>
     </p>
